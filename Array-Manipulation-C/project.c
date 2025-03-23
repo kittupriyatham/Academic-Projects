694c37ec7e01017b76aa26c5b31f6a2a23e65f4e
Krishna Priyatham Potluri <kittu.priyatham@gmail.com>
2025-03-23 11:10:58 +0530

    Add global array `int arr[100]` and modify functions to use it
    * Add global array `int arr[100]` to be used throughout the program
    * Modify `insert`, `deletion`, `searching`, and `sorting` functions to use the global array `arr`
    * Add function `void display_array(int n)` to display the contents of the array
    * Add function `void get_user_input(int *n)` to get user input for the array
    * Add error handling for invalid inputs in `insert`, `deletion`, `searching`, and `sorting` functions

--- Array-Manipulation-C/project.c
+++ Array-Manipulation-C/project.c
@@ -1,238 +1,179 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-int insert();
+// Global array to be used throughout the program
+int arr[100];
 
-int deletion();
-
-int searching();
-
-int sorting();
+// Function prototypes
+int insert(int *n);
+int deletion(int *n);
+int searching(int n);
+int sorting(int n);
+void clear_screen();
+void display_array(int n);
+void get_user_input(int *n);
 
 int main ()
-
 {
- 
- int c,s;	
+    int c, s, n = 0;
 
-while (1)
-
-{
-	
-printf("enter choice\n");
-
-printf("1.insertion\n");
-
-printf("2.deletion\n");
-
-printf("3.searching\n");
-
-printf("4.sorting\n");
-
-printf("5.exiting\n");
-
-scanf("%d",&c);
-
-switch (c)
-
-{
-	
-case 1: insert();
-
-    break;
-    
-case 2: deletion();
-
-    break;   
-    
-case 3:  searching();
-
-    break; 
-
-case 4:  sorting();
-
-    break; 
-case 5:  exit(1);
-
-    break;   
-}
-
-printf("enter any number to clear the screen\n");
-
-scanf("%d",&s);
-
-	system("cls");  
-}
-
-}
-
-
-
-
-int insert()
-{
- 	
-  int a[100],p,i,k,j,x,n;
-  
-  
-  printf("Enter number of elements in array\n");
-  
-  
-   scanf("%d", &n);
-   
-   
-  printf("Enter %d elements\n", n);
-  
-  
-   for (i=0;i<n;i++)
-   
-      scanf("%d", &a[i]);
-      
-      
-      
-   printf("Enter the location where you wish to insert an element\n");
-   
-   scanf("%d", &p);
-   
- 
-   printf("Enter the value to insert\n");
-   
-   scanf("%d", &x);
- 
-   for (k=n-1;k>=p-1;k--)
-   
-      a[k+1] = a[k];
- 
-   a[p-1]=x;
- 
-   printf("Resultant array is\n");
- 
-   for (j=0;j<=n;j++)
-   
-      printf("%d\n", a[j]);
-      
-      return 0;
-      
- }
- 
- 
- 
- 
- 
-int deletion()
-{
- 	
-  int a[100],p,i,k,j,n;
-  
-  printf("Enter number of elements in array\n");
-  
-   scanf("%d", &n);
-   
-  printf("Enter %d elements\n", n);
-  
-  
-   for (i=0;i<n;i++)
-   
-      scanf("%d", &a[i]);
-      
-      
-   printf("Enter the location where you wish to delete element\n");
-   
-   scanf("%d", &p);
- 
-   if (p>= n+1)
-   
-      printf("Deletion not possible.\n");
-      
-   else
+    while (1)
     {
-      for (k=p-1;k<n-1;k++)
-      
-         a[k] = a[k+1];
- 
-      printf("Resultant array:\n");
- 
-      for (j=0;j<n-1;j++)
-      
-         printf("%d\n",a[j]);
-         
-   }
-   
-   return 0;
+        printf("Enter choice\n");
+        printf("1. Insertion\n");
+        printf("2. Deletion\n");
+        printf("3. Searching\n");
+        printf("4. Sorting\n");
+        printf("5. Exiting\n");
+
+        scanf("%d", &c);
+
+        switch (c)
+        {
+            case 1: insert(&n);
+                break;
+            case 2: deletion(&n);
+                break;
+            case 3: searching(n);
+                break;
+            case 4: sorting(n);
+                break;
+            case 5: exit(1);
+                break;
+            default: printf("Invalid choice. Please try again.\n");
+        }
+
+        printf("Enter any number to clear the screen\n");
+        scanf("%d", &s);
+        clear_screen();
+    }
 }
 
-
-
-
-int searching()
+// Function to insert an element into the array
+int insert(int *n)
 {
-	int a[100],n,i,num,pos,found;
-	
-	 printf("Enter number of elements in array\n");
-  
-   scanf("%d", &n);
-   
-  printf("Enter %d elements\n", n);
-  
-  
-   for (i=0;i<n;i++)
-   
-      scanf("%d", &a[i]);
-    
-	  printf("Enter the number to be searched\n");
-    scanf("%d",&num);
-    for (i=0;i<n;i++)
-    {
-    	if(a[i]==num)
-    	{
-    		found=1;
-    		pos=i;
-    		printf("%d is found in array at position %d\n",num,i+1);
-    		break;
-		}
-		else
-		{
-			found=0;
-		}
-	}
-	if (found==0)
-	{
-		printf("%d doesnt exist in array\n",num);
-		return 0;      
-	}
+    int p, i, k, x;
+
+    printf("Enter the location where you wish to insert an element\n");
+    scanf("%d", &p);
+
+    if (p <= 0 || p > *n + 1) {
+        printf("Invalid location. Please enter a location between 1 and %d.\n", *n + 1);
+        return -1;
+    }
+
+    printf("Enter the value to insert\n");
+    scanf("%d", &x);
+
+    for (k = *n - 1; k >= p - 1; k--)
+        arr[k + 1] = arr[k];
+
+    arr[p - 1] = x;
+    (*n)++;
+
+    printf("Resultant array is\n");
+    display_array(*n);
+
+    return 0;
 }
 
-
-
-
-int sorting()
+// Function to delete an element from the array
+int deletion(int *n)
 {
-	int a[100],n,i,j,temp;
-	 printf("Enter number of elements in array\n");
-  
-   scanf("%d", &n);
-   
-  printf("Enter %d elements\n", n);
-  
-  
-   for (i=0;i<n;i++)
-   scanf("%d", &a[i]);
-   for(i=0;i<n-1;i++)
-    {
-      	for(j=0;j<n-i-1;j++)
-      	{
-      		if(a[j]>a[j+1])
-      		{
-      		temp=a[j];
-			  a[j]=a[j+1];
-			  a[j+1]=temp;	
-			}
-		}
-	}
-	printf("the array sorted in ascending order is:\n");
-	for(i=0;i<n;i++)
-	{
-		printf("%d\n",a[i]);
-	}
-	return 0;
+    int p, k;
+
+    printf("Enter the location where you wish to delete element\n");
+    scanf("%d", &p);
+
+    if (p <= 0 || p > *n) {
+        printf("Invalid location. Please enter a location between 1 and %d.\n", *n);
+        return -1;
+    }
+
+    for (k = p - 1; k < *n - 1; k++)
+        arr[k] = arr[k + 1];
+
+    (*n)--;
+
+    printf("Resultant array:\n");
+    display_array(*n);
+
+    return 0;
+}
+
+// Function to search for an element in the array
+int searching(int n)
+{
+    int i, num, pos, found = 0;
+
+    printf("Enter the number to be searched\n");
+    scanf("%d", &num);
+
+    for (i = 0; i < n; i++) {
+        if (arr[i] == num) {
+            found = 1;
+            pos = i;
+            printf("%d is found in array at position %d\n", num, i + 1);
+            break;
+        }
+    }
+
+    if (!found)
+        printf("%d doesn't exist in array\n", num);
+
+    return 0;
+}
+
+// Function to sort the array in ascending order
+int sorting(int n)
+{
+    int i, j, temp;
+
+    for (i = 0; i < n - 1; i++) {
+        for (j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+
+    printf("The array sorted in ascending order is:\n");
+    display_array(n);
+
+    return 0;
+}
+
+// Function to clear the screen in a platform-independent way
+void clear_screen()
+{
+#ifdef _WIN32
+    system("cls");
+#else
+    system("clear");
+#endif
+}
+
+// Function to display the array
+void display_array(int n)
+{
+    for (int i = 0; i < n; i++)
+        printf("%d\n", arr[i]);
+}
+
+// Function to get user input for the array
+void get_user_input(int *n)
+{
+    printf("Enter number of elements in array\n");
+    scanf("%d", n);
+
+    if (*n <= 0 || *n > 100) {
+        printf("Invalid number of elements. Please enter a number between 1 and 100.\n");
+        return;
+    }
+
+    printf("Enter %d elements\n", *n);
+    for (int i = 0; i < *n; i++)
+        scanf("%d", &arr[i]);
 }
